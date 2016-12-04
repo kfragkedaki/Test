55e243ec8e92234f66af87296d472b5556c6d503
klairh <klairh>
2016-12-04 22:41:10 +0200

    Modify StudentApplication

--- StudentApplication.java
+++ StudentApplication.java
@@ -1,37 +1,35 @@
 import java.util.Scanner;
-public class  StudentApplication
-{
-public void main(String[] args)
-{String stdam;
-String stdname;
-do
-{Student firststudent=new Student("11111111","Jim Moris");
-Student secondstudent=new Student("22222222","Tom Burn");
-System.out.println("--- MENU ---\n1.Print all Students\n2.Insert new Student\n3.Find a Student\n4.Delete a Student\n5.Exit Program\n---------------");
-Scanner input=new Scanner(System.in);
-int x=input.nextInt();
-if (x==1)
-Student.printStudents();
-else if (x==2)
-{System.out.println("Πρόσθεσε το AM: " );
-stdam=Student.getAm();
-System.out.println("Πρόσθεσε το όνομα του μαθητή:");
-stdname=Student.getName();
-Student student=new Student(stdam,stdname);
-}
-else if (x==3)
-{
-System.out.println("Πρόσθεσε το AM: " );
-stdam=Student.getAm();
-Student.findStudentData(stdam);
-}
-else if (x==4)
-{System.out.println("Πρόσθεσε το AM: " );
-stdam=Student.getAm();
-Student.deleteStudent(stdam);
-}
-else
-break;}
-while (x!=5);
-}
+
+public class  StudentApplication {
+	public void main(String[] args) {
+		String stdam;
+		String stdname;
+		do {
+			Student firststudent=new Student("11111111","Jim Moris");
+			Student secondstudent=new Student("22222222","Tom Burn");
+			System.out.println("--- MENU ---\n1.Print all Students\n2.Insert new Student\n3.Find a Student\n4.Delete a Student\n5.Exit Program\n---------------");
+			Scanner input=new Scanner(System.in);
+			int x=input.nextInt();
+			if (x==1) Student.printStudents();
+			else if (x==2) {
+ 				System.out.println("Πρόσθεσε το AM: " );
+				stdam=Student.getAm();
+				System.out.println("Πρόσθεσε το όνομα του μαθητή:");
+				stdname=Student.getName();
+				Student student=new Student(stdam,stdname);
+			}
+		 	else if (x==3) {
+				System.out.println("Πρόσθεσε το AM: " );
+				stdam=Student.getAm();
+				Student.findStudentData(stdam);
+			}
+			else if (x==4) {
+				System.out.println("Πρόσθεσε το AM: " );
+				stdam=Student.getAm();
+				Student.deleteStudent(stdam);
+			}
+			else break;
+			}
+		while (x!=5);
+	}
 }
