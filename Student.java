fb0bca100c5175808c5bf749db17c0fa7268341a
klairh <klairh>
2016-12-04 22:45:35 +0200

    Compile Student

--- Student.java
+++ Student.java
@@ -56,25 +56,3 @@ public class Student {
 }
 
 
-	return Student.get(i).toString;
-f=1;}
-i=i+1;
-}
-if (f==0) return "Sorry the Student with AM: «όρισμα» is not registered in our database";
-}
-public static void deleteStudent(String stdam)
-{
-int i=0,f=0;
-while (i<students.size())
-{ if (students.get(i).getAm().equals(stdam))
-     {students.remove(i);
-      f=1;}
-i=i+1;}
-if (f==0) System.out.println("Sorry the Student with AM: «όρισμα» is not registered in our database");
-}
-public static void printStudents()
-{if (students.size()==0) System.out.println("No Students have been registered yet!");
-else
-for (int i=0;i<students.size();i++) System.out.println(students.get(i));
-}
-}
