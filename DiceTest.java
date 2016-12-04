aea8118ff5144044056cd44057b2e22296860f8e
klairh <klairh>
2016-12-04 22:36:28 +0200

    Modify DiceTest

--- DiceTest.java
+++ DiceTest.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-public class DiceTest
-{
-public static void main(String args[])
-{
-System.out.println("Eισάγετε έναν θετικό ακέραιο αριθμό");
-Scanner input=new Scanner(System.in);
-int n=input.nextInt();
-Dice game=new Dice();
-for(int i=1;i<=n;i++)
-{int x=game.roll();
-game.updateFrequency(x);
-}
-game.printFrequencies();
-}
+
+public class DiceTest {
+	public static void main(String args[]) {
+	    System.out.println("Eισάγετε έναν θετικό ακέραιο αριθμό");
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		Dice game=new Dice();
+		for(int i=1;i<=n;i++) {
+			int x=game.roll();
+			game.updateFrequency(x);
+		}
+		game.printFrequencies();
+	}
 }
