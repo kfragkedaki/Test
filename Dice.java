7cec40d17c8b390c901fa194ae565eb3faa96f27
klairh <klairh>
2016-12-04 22:35:27 +0200

    Modify Dice

--- Dice.java
+++ Dice.java
@@ -1,23 +1,21 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Dice
-{
-private int[] frequency=new int[6];
 
-public int roll()
-{
-Random randomNumbers = new Random();
-int x;
-x=randomNumbers.nextInt(6);
-return x;
-}
+public class Dice {
+    private int[] frequency=new int[6];
 
-public void updateFrequency(int x)
-{frequency[x]++;
-}
+    public int roll() {
+        Random randomNumbers = new Random();
+        int x;
+        x=randomNumbers.nextInt(6);
+        return x;
+    }
 
-public void printFrequencies()
-{
-System.out.printf("Πόσες φορές εμφανίστηκε κάθε αριθμος:\n1:%d\n2:%d\n3:%d\n4:%d\n5:%d\n6:%d\n",frequency[0],frequency[1],frequency[2],frequency[3],frequency[4],frequency[5]);}
-}
+	public void updateFrequency(int x) {
+	frequency[x]++;
+	}
+
+ 	public void printFrequencies() {
+	System.out.printf("Πόσες φορές εμφανίστηκε κάθε αριθμος:\n1:%d\n2:%d\n3:%d\n4:%d\n5:%d\n6:%d\n",frequency[0],frequency[1],frequency[2],frequency[3],frequency[4],frequency[5]);}
+	}
 
